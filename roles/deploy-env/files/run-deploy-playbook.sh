8cfdbac035da178a6e77ed2f51e0ac87726c04e4
d.pasquali <d.pasquali@reply.it>
2018-02-25 14:34:30 +0100

    deploy folders and playbook

---
+++ roles/deploy-env/files/run-deploy-playbook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$1" == "localhost"]
+    ansible-playbook -i ./hosts --connection=local deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
+else
+    ansible-playbook -i '$1,' deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
+fi
