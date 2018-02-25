2e5c92cde65c9baed0daab36716d29e0e3c18be1
d.pasquali <d.pasquali@reply.it>
2018-02-25 14:44:05 +0100

    fix error in launch script

--- roles/deploy-env/files/run-deploy-playbook.sh
+++ roles/deploy-env/files/run-deploy-playbook.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$1" == "localhost"]
+if [ "$1" == "localhost"]; then
     ansible-playbook -i ./hosts --connection=local deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
 else
     ansible-playbook -i '$1,' deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
