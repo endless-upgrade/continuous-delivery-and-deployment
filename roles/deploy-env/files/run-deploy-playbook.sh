e8d269c84bcd4a0287805dcc7165dd98362fac44
d.pasquali <d.pasquali@reply.it>
2018-02-25 15:54:50 +0100

    fix run script

--- roles/deploy-env/files/run-deploy-playbook.sh
+++ roles/deploy-env/files/run-deploy-playbook.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+base=`dirname $0`
+
 if [ "$1" == "localhost" ]; then
-    ansible-playbook -i ./hosts --connection=local deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
+    ansible-playbook -i "$base/hosts" --connection=local  "$base/deploy.yml" -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
 else
-    ansible-playbook -i "$1," deploy.yml -e 'host_key_checking=False' --private-key=ssh/private/key/path -e 'ansible_ssh_user=dario_pasquali93' --extra-vars "SRC=$2 DEST=$3"
+    ansible-playbook -i "$1," "$base/deploy.yml" -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
 fi
