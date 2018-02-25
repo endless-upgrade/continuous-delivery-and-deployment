d329fde4a6d9e18cc90a7fcabfd7d579e7c87f9a
d.pasquali <d.pasquali@reply.it>
2018-02-25 15:23:21 +0100

    remote deploy with ssh and user

--- roles/deploy-env/files/run-deploy-playbook.sh
+++ roles/deploy-env/files/run-deploy-playbook.sh
@@ -3,5 +3,5 @@
 if [ "$1" == "localhost" ]; then
     ansible-playbook -i ./hosts --connection=local deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
 else
-    ansible-playbook -i "$1," deploy.yml -e 'host_key_checking=False' --extra-vars "SRC=$2 DEST=$3"
+    ansible-playbook -i "$1," deploy.yml -e 'host_key_checking=False' --private-key=ssh/private/key/path -e 'ansible_ssh_user=dario_pasquali93' --extra-vars "SRC=$2 DEST=$3"
 fi
